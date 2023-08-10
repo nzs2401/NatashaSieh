9a51646276f4f48c01981710f235a43c5340798c
nzs2401 <116852829+nzs2401@users.noreply.github.com>
2023-08-09 18:35:04 -1000

    Create facedetection.py

---
+++ SEAP/facedetection.py
@@ -0,0 +1,34 @@
+import cv2
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface.xml")
+
+def detect_face(img):
+
+    face_img = img.copy()
+
+    face_rects = face_cascade.detectMultiScale(face_img)
+
+    for (x,y,w,h) in face_rects:
+        cv2.rectangle(face_img,(x,y),(x+w,y+h),(255,255,255),10)
+
+    return face_img
+
+cap = cv2.VideoCapture(0)
+
+while True:
+
+    ret,frame = cap.read()
+
+    frame = detect_face(frame)
+
+    cv2.imshow('Video Face Detect',frame)
+
+    k = cv2.waitKey(1)
+    if k == 27:
+        break
+
+cap.release()
+cap.destroyAllWindows()
